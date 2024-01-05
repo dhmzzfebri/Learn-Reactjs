dfe6ecb60bbbc0dfe6fa42b51a8b67c73765f935
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-01-05 13:05:16 +0700

    feat: remove unused code

--- components/navbar/menu.tsx
+++ components/navbar/menu.tsx
@@ -60,7 +60,9 @@ export default function Menu({ handleShow, condition }: HamburgerProps) {
           </ListLink>
 
           <ContactContainer className={inter.className}>
-            <Link href={'contact'}>Lets Talk</Link>
+            <Link data-testid="dekstop-contact" href={'contact'}>
+              Lets Talk
+            </Link>
           </ContactContainer>
         </MenuContainer>
 
