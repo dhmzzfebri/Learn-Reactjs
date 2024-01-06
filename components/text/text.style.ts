cd18d29d9717de6a035f18246b291a70c654e447
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-01-06 16:18:35 +0700

    refactor: remove unused props

--- components/text/text.style.ts
+++ components/text/text.style.ts
@@ -5,14 +5,9 @@ export const TextComponent = styled.p<{
   $color: string;
   $family: string;
   $weight: string;
-  $mobile_size: string;
 }>`
   font-family: ${(props) => props.$family}, serif;
   font-size: ${(props) => props.$size};
   color: ${(props) => props.$color};
   font-weight: ${(props) => props.$weight};
-  @media (width <= 588px) {
-    font-size: ${(props) =>
-      props.$mobile_size !== '' ? props.$mobile_size : props.$size};
-  }
 `;
