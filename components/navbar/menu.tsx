452bf0c107e2f39bf04b8658306b0a26677d332b
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-01-06 16:17:28 +0700

    refactor: move component

--- components/navbar/menu.tsx
+++ components/navbar/menu.tsx
@@ -17,63 +17,49 @@ import { lato } from '@/app/fonts';
 import { inter } from '@/app/fonts';
 
 interface HamburgerProps {
-  handleShow: () => void;
   condition: boolean;
 }
 
-export default function Menu({ handleShow, condition }: HamburgerProps) {
+export default function Menu({ condition }: HamburgerProps) {
   const pathName = usePathname();
 
   return (
     <>
-      <NavWrapper>
-        <TitleContainer>
-          <Link href={'/'}>
-            <h1 className={lato.className}>Digi Dreams</h1>
-          </Link>
-        </TitleContainer>
+      <TitleContainer>
+        <Link href={'/'}>
+          <h1 className={lato.className}>Digi Dreams</h1>
+        </Link>
+      </TitleContainer>
 
-        <MenuContainer $isShow={condition}>
-          <ListLink className={inter.className}>
-            <ListItem $isSelect={pathName === '/'}>
-              <Link href={'/'}>Home</Link>
-            </ListItem>
+      <MenuContainer $isShow={condition}>
+        <ListLink className={inter.className}>
+          <ListItem $isSelect={pathName === '/'}>
+            <Link href={'/'}>Home</Link>
+          </ListItem>
 
-            <ListItem $isSelect={pathName === '/about'}>
-              <Link href={'/about'}>About Us</Link>
-            </ListItem>
+          <ListItem $isSelect={pathName === '/about'}>
+            <Link href={'/about'}>About Us</Link>
+          </ListItem>
 
-            <ListItem $isSelect={pathName === '/solutions'}>
-              <Link href={'/solutions'}>Solutions</Link>
-            </ListItem>
+          <ListItem $isSelect={pathName === '/solutions'}>
+            <Link href={'/solutions'}>Solutions</Link>
+          </ListItem>
 
-            <ListItem $isSelect={pathName === '/blog'}>
-              <Link href={'/blog'}>Blog</Link>
-            </ListItem>
+          <ListItem $isSelect={pathName === '/blog'}>
+            <Link href={'/blog'}>Blog</Link>
+          </ListItem>
 
-            <ListItem $isSelect={pathName === '/internship'}>
-              <Link href={'/internship'}>Internship</Link>
-            </ListItem>
-          </ListLink>
+          <ListItem $isSelect={pathName === '/internship'}>
+            <Link href={'/internship'}>Internship</Link>
+          </ListItem>
+        </ListLink>
 
-          <ContactContainer className={inter.className}>
-            <Link data-testid="mobile-contact" href={'contact'}>
-              Lets Talk
-            </Link>
-          </ContactContainer>
-        </MenuContainer>
-
-        <HamburgerContainer className={inter.className}>
-          <Link data-testid="dekstop-contact" href={'contact'}>
+        <ContactContainer className={inter.className}>
+          <Link data-testid="mobile-contact" href={'contact'}>
             Lets Talk
           </Link>
-          <HamburgerIcon data-testid="hamburger-icon" onClick={handleShow}>
-            <FirstSpan $isShow={condition}></FirstSpan>
-            <SecondSpan $isShow={condition}></SecondSpan>
-            <ThirdSpan $isShow={condition}></ThirdSpan>
-          </HamburgerIcon>
-        </HamburgerContainer>
-      </NavWrapper>
+        </ContactContainer>
+      </MenuContainer>
     </>
   );
 }
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,6 +2,15 @@ import { useState } from 'react';
 import Container from '../containerComponents/container';
 import PaddingContainer from '../paddingContainer/paddingContainer';
 import Menu from './menu';
+import {
+  NavWrapper,
+  HamburgerContainer,
+  HamburgerIcon,
+  FirstSpan,
+  SecondSpan,
+  ThirdSpan,
+} from './navbar.style';
+import Link from 'next/link';
 
 export default function Navbar() {
   const [isShow, setIsShow] = useState(false);
@@ -13,7 +22,19 @@ export default function Navbar() {
   return (
     <Container element="nav" position="fixed">
       <PaddingContainer childContainer={false}>
-        <Menu handleShow={handleShow} condition={isShow} />
+        <NavWrapper>
+          <Menu condition={isShow} />
+          <HamburgerContainer>
+            <Link data-testid="dekstop-contact" href={'contact'}>
+              Lets Talk
+            </Link>
+            <HamburgerIcon data-testid="hamburger-icon" onClick={handleShow}>
+              <FirstSpan $isShow={isShow}></FirstSpan>
+              <SecondSpan $isShow={isShow}></SecondSpan>
+              <ThirdSpan $isShow={isShow}></ThirdSpan>
+            </HamburgerIcon>
+          </HamburgerContainer>
+        </NavWrapper>
       </PaddingContainer>
     </Container>
   );
