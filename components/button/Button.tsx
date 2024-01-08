ee605f69ee924c0b5b63433fc7224a4c7a911c9e
Roid-obi <contohdiamazon2@gmail.com>
2024-01-08 14:08:11 +0700

    feat: create component button

---
+++ components/button/Button.tsx
@@ -0,0 +1,52 @@
+'use client';
+import Link from 'next/link';
+import { StyledButton } from './Button.style';
+import { useState } from 'react';
+
+export interface ButtonProps {
+  /**
+   * Enter the button type "primary" or "secondary"
+   */
+  Type: 'primary' | 'secondary';
+  /**
+   * Enter content button
+   */
+  Content: string;
+  /**
+   * Leading to where
+   */
+  href?: string;
+  /**
+   * Enter additional styles
+   */
+  style?: React.CSSProperties;
+  /**
+   * Action
+   */
+  onClick?: () => void;
+}
+
+export default function Button({
+  Type = 'primary',
+  Content,
+  href,
+  style,
+  ...props
+}: ButtonProps) {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const mode = `button--${Type}`;
+  return (
+    <>
+      <Link href={href || '/'}>
+        <StyledButton
+          className={['button', mode].join(' ')}
+          disabled={isDisabled}
+          style={style}
+          {...props}
+        >
+          {Content}
+        </StyledButton>
+      </Link>
+    </>
+  );
+}
